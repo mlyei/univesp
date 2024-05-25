372500c781f446ed0eb292afa67e8a5148bf1913
Emily <124410773+mlyei@users.noreply.github.com>
2024-05-25 15:50:36 +0000

    #adição do node.js

--- ProjetoIntegrador1/config.py
+++ ProjetoIntegrador1/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_key"
-    SQLALCHEMY_DATABASE_URI = 'mysql://usuario:senha@ip/nomedabase'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:123@192.168.1.4:3336/PlataformaAEE'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
