2003c6fc0bf12374f2b16e78ee3edd8ef9c6220f
Emily <124410773+mlyei@users.noreply.github.com>
2024-05-26 15:04:13 +0000

    #mudança logica formulário do objeto aluno e do objeto escola #mudança nas rotas dos arquivos #inserção dos formularios no arquivo HTML

--- ProjetoIntegrador1/smcnd/aluno/routes.py
+++ ProjetoIntegrador1/smcnd/aluno/routes.py
@@ -1,19 +1,64 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from smcnd.models import Aluno
 from smcnd import db
+from datetime import datetime
 
 bp = Blueprint('aluno', __name__)
 
 @bp.route('/aluno', methods=['GET', 'POST'])
 def manage_alunos():
     if request.method == 'POST':
-        # Lógica para processar o formulário de cadastro de alunos
         nome = request.form.get('nome')
-        # ... outros campos
-        novo_aluno = Aluno(nome=nome)
+        ra = request.form.get('ra')
+        data_de_nascimento = request.form.get('data_de_nascimento')
+        responsavel = request.form.get('responsavel')
+        telefone = request.form.get('telefone')
+        endereco = request.form.get('endereco')
+        bairro = request.form.get('bairro')
+        cidade = request.form.get('cidade')
+        usuarios_idusuarios = request.form.get('usuarios_idusuarios')
+        escola_idescola = request.form.get('escola_idescola')
+        professor_idprofessor = request.form.get('professor_idprofessor')
+        assistente_educacional_idassistente_educacional = request.form.get('assistente_educacional_idassistente_educacional')
+        aee_acompanhamento_idaee = request.form.get('aee_acompanhamento_idaee')
+
+        novo_aluno = Aluno(
+                Nome_aluno=nome,
+                RA=ra,
+                Data_de_Nascimento=datetime.strptime(data_de_nascimento, '%Y-%m-%d'),
+                Responsavel=responsavel,
+                Telefone=telefone,
+                Endereco=endereco,
+                Bairro=bairro,
+                Cidade=cidade,
+                Usuarios_idUsuarios=usuarios_idusuarios,
+                Escola_idEscola=escola_idescola,
+                Professor_idProfessor=professor_idprofessor,
+                Assistente_Educacional_idAssistente_Educacional=assistente_educacional_idassistente_educacional,
+                AEE_acompanhamento_idAEE=aee_acompanhamento_idaee
+        )
         db.session.add(novo_aluno)
         db.session.commit()
         return redirect(url_for('aluno.manage_alunos'))
     
     alunos = Aluno.query.all()
     return render_template('aluno.html', alunos=alunos)
+
+@bp.route('/aluno/<int:id_aluno>/detalhes', methods=['GET'])
+def detalhes_aluno(id_aluno):
+    aluno = Aluno.query.get_or_404(id_aluno)
+    return jsonify({
+        'Nome_aluno': aluno.Nome_aluno,
+        'RA': aluno.RA,
+        'Data_de_Nascimento': aluno.Data_de_Nascimento.isoformat(),
+        'Responsavel': aluno.Responsavel,
+        'Telefone': aluno.Telefone,
+        'Endereco': aluno.Endereco,
+        'Bairro': aluno.Bairro,
+        'Cidade': aluno.Cidade,
+        'Usuarios': aluno.Usuarios_idUsuarios,
+        'Escola': aluno.Escola_idEscola,
+        'Professor': aluno.Professor_idProfessor,
+        'Assistente_Educacional': aluno.Assistente_Educacional_idAssistente_Educacional,
+        'Aee_acompanhamento': aluno.AEE_acompanhamento_idAEE
+    })
--- ProjetoIntegrador1/smcnd/escola/routes.py
+++ ProjetoIntegrador1/smcnd/escola/routes.py
@@ -1,7 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from smcnd.models import Escola
+from smcnd import db
 
 bp = Blueprint('escola', __name__,)
 
-@bp.route('/escola')
-def escola_home():
-    return render_template('escola.html')
+@bp.route('/escola', methods=['GET', 'POST'])
+def manage_escola():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        ensino_fundamental = request.form.get('ensino_fundamental')
+        turmas = request.form.get('turmas')
+        manha = request.form.get('manha')
+        tarde = request.form.get('tarde')
+
+        nova_escola = Escola(
+            Nome=nome,
+            Ensino_Fundamental=ensino_fundamental == 'on',
+            Turmas=turmas,
+            Manha=manha == 'on',
+            Tarde=tarde == 'on'
+        )
+
+        db.session.add(nova_escola)
+        db.session.commit()
+        return redirect(url_for('escola.manage_escola'))
+
+escolas = Escola.query.all()
+return render_template('escola.html')
+
+@bp.route('/escola/<int:id_escola>/detalhes', methods=['GET'])
+def detalhes_escola(id_escola):
+    escola = Escola.query.get_or_404(id_escola)
+    return jsonify({
+        'Nome': escola.Nome,
+        'Ensino_Fundamental': escola.Ensino_Fundamental,
+        'Turmas': escola.Turmas,
+        'Manha': escola.Manha,
+        'Tarde': escola.Tarde
+    })
